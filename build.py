5c3fb5f3120684a8048264ac7f907e394791dcb4
Ryuji TSUTSUI <ryu22e@gmail.com>
2023-08-09 22:17:49 +0900

    Extract the code to save the image in build.py as a function

--- build.py
+++ build.py
@@ -1,23 +1,14 @@
 #!/usr/bin/env python
 import argparse
-from typing import TypeAlias
+from pathlib import Path
 
-import matplotlib.pyplot as plt
-from japanmap import picture
-
+from postersession.japanmap import save_image
 from postersession.pyconjp import get_prefs
-
-Color: TypeAlias = tuple[int, int, int]
-COLOR_HELD_PREF: Color = (119, 197, 228)
-ImageSize: TypeAlias = float
-DPI: ImageSize = 300.0
 
 
 def main(output: str) -> None:
     prefs = get_prefs()
-    pct = picture({pref: COLOR_HELD_PREF for pref in prefs})
-    plt.imshow(pct)
-    plt.savefig(output, dpi=DPI)
+    save_image(prefs, Path(output))
 
 
 if __name__ == "__main__":
