7b261022df01aefec7d3e04434b4781c56aea837
Ryuji TSUTSUI <ryu22e@gmail.com>
2023-08-10 23:43:39 +0900

    Support for caching on site request

--- postersession/pyconjp.py
+++ postersession/pyconjp.py
@@ -2,13 +2,29 @@ from collections.abc import Iterator
 
 import requests
 from bs4 import BeautifulSoup
+from requests_cache import CachedSession
 
 URL = "https://www.pycon.jp/support/bootcamp.html"
 USER_AGENT = "pyconapac2023-postersession-japanmap/0.1.0"
 
 
-def get_prefs() -> Iterator[str]:
-    r = requests.get(URL, headers={"User-Agent": USER_AGENT})
+def _get_session(
+    cache_directory: str | None,
+) -> requests.Session | CachedSession:
+    session: requests.Session | CachedSession
+    if cache_directory:
+        session = CachedSession(
+            cache_directory, backend="filesystem", serializer="json"
+        )
+    else:
+        session = requests.Session()
+    session.headers["User-Agent"] = USER_AGENT
+    return session
+
+
+def get_prefs(cache_directory: str | None = None) -> Iterator[str]:
+    session = _get_session(cache_directory)
+    r = session.get(URL)
     r.raise_for_status()
 
     soup = BeautifulSoup(r.text, "html.parser")
--- tests/postersession/test_pyconjp.py
+++ tests/postersession/test_pyconjp.py
@@ -51,3 +51,60 @@ def test_get_prefs(html, responses):
         ]
     )
     assert actual == expected
+
+
+def test_get_prefs_from_cache(html, responses, tmp_path):
+    from postersession.pyconjp import get_prefs
+
+    responses.add(
+        responses.GET,
+        "https://www.pycon.jp/support/bootcamp.html",
+        body=html.read("pyconjp.html"),
+    )
+    cache_directory = tmp_path
+
+    actual = sorted(list(get_prefs(cache_directory)))
+
+    expected = sorted(
+        [
+            "北海道",
+            "岩手",
+            "宮城",
+            "山形",
+            "福島",
+            "茨城",
+            "栃木",
+            "群馬",
+            "埼玉",
+            "千葉",
+            "神奈川",
+            "山梨",
+            "長野",
+            "新潟",
+            "富山",
+            "石川",
+            "福井",
+            "静岡",
+            "愛知",
+            "岐阜",
+            "京都",
+            "大阪",
+            "兵庫",
+            "和歌山",
+            "岡山",
+            "広島",
+            "山口",
+            "香川",
+            "愛媛",
+            "徳島",
+            "高知",
+            "福岡",
+            "佐賀",
+            "長崎",
+            "熊本",
+            "鹿児島",
+            "沖縄",
+        ]
+    )
+    assert actual == expected
+    assert cache_directory.exists()
