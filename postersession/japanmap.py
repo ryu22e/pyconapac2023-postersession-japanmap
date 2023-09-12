ae62a68e45e8cebcc019079c1aa8c4413023a269
Ryuji TSUTSUI <ryu22e@gmail.com>
2023-09-12 13:57:42 +0900

    Adjust map margins

--- postersession/japanmap.py
+++ postersession/japanmap.py
@@ -14,5 +14,5 @@ def save_image(prefs: Iterable, output: Path) -> Path:
     pct = picture({pref: COLOR_HELD_PREF for pref in prefs})
     plt.axis("off")
     plt.imshow(pct)
-    plt.savefig(output, dpi=DPI)
+    plt.savefig(output, dpi=DPI, facecolor="azure", bbox_inches="tight", pad_inches=0)
     return output
